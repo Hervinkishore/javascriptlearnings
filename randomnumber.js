58d316104248879d9589ad1a7df68a80ec8a175c
Hervinkishore <hervinkishorecr7@gmail.com>
2026-08-20 20:53:25 +0530

    this updates the random number code

--- randomnumber.js
+++ randomnumber.js
@@ -1,6 +1,8 @@
+const MIN=50
+const MAX=100
 let number=Math.random();
-document.getElementById("randnum").textContent=number
+document.getElementById("randnum").textContent=Math.floor(Math.random()*(MAX-MIN))+MIN
 
 document.getElementById("generate").onclick=function(){
-    document.getElementById("randnum").textContent=Math.random()
+    document.getElementById("randnum").textContent=Math.floor(Math.random()*(MAX-MIN))+MIN
 }
